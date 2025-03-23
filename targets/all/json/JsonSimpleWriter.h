c0d8dd50a25ab4958430c3375f7f11ddccd5b991
Štefan Šimek <simek@triaxis.sk>
2025-03-23 17:47:07 +0100

    fix: JsonSimpleWriter - aggressive inlining to avoid generating lots of useless temporaries in debug builds

--- targets/all/json/JsonSimpleWriter.h
+++ targets/all/json/JsonSimpleWriter.h
@@ -23,19 +23,37 @@ public:
 
     void PathKey(const char* path, const char* comment = NULL);
 
-    void Null() { wr.Null(); }
-    void Float(float f) { wr.Float(f); }
-    void Bool(bool b) { wr.Bool(b); }
-    void Int(int n) { wr.Int(n); }
-    void String(Span s) { wr.String(s.Pointer(), s.Length()); }
+    ALWAYS_INLINE void Null() { wr.Null(); }
+    ALWAYS_INLINE void Float(float f) { wr.Float(f); }
+    ALWAYS_INLINE void Bool(bool b) { wr.Bool(b); }
+    ALWAYS_INLINE void Int(int n) { wr.Int(n); }
+    ALWAYS_INLINE void String(Span s) { wr.String(s.Pointer(), s.Length()); }
 
-    template<typename T> void SingleOrArray(Span s)
+    ALWAYS_INLINE void StartObject() { wr.StartObject(); }
+    ALWAYS_INLINE void StartObject(Span key) { Key(key); StartObject(); }
+    ALWAYS_INLINE void EndObject() { wr.EndObject(); }
+
+    ALWAYS_INLINE void StartArray() { wr.StartArray(); }
+    ALWAYS_INLINE void StartArray(Span key) { Key(key); StartArray(); }
+    ALWAYS_INLINE void EndArray() { wr.EndArray(); }
+
+    ALWAYS_INLINE void Key(Span key) { wr.Key(key.Pointer(), key.Length()); }
+    ALWAYS_INLINE void CommentLine(Span comment) { wr.CommentLine(comment.Pointer(), comment.Length()); }
+    ALWAYS_INLINE void CommentInline(Span comment) { wr.CommentInline(comment.Pointer(), comment.Length()); }
+
+    template<typename T> ALWAYS_INLINE void Value(Span key, const T& value)
+    {
+        Key(key);
+        Value<T>(value);
+    }
+
+    template<typename T> ALWAYS_INLINE void SingleOrArray(Span s)
     {
         if (s.Length() / sizeof(T) == 1) { Value(s.Element<T>()); }
         else { Array<T>(s); }
     }
 
-    template<typename T> void Array(Span s)
+    template<typename T> ALWAYS_INLINE void Array(Span s)
     {
         size_t len = s.Length() / sizeof(T);
         wr.StartArray();
@@ -55,8 +73,11 @@ private:
     const char* UpdatePath(const char* path);
 };
 
-template<> inline void JsonSimpleWriter::Value<float>(const float& v) { Float(v); }
-template<> inline void JsonSimpleWriter::Value<int>(const int& i) { Int(i); }
-template<> inline void JsonSimpleWriter::Value<bool>(const bool& b) { Bool(b); }
+template<> ALWAYS_INLINE void JsonSimpleWriter::Value<float>(const float& v) { Float(v); }
+template<> ALWAYS_INLINE void JsonSimpleWriter::Value<unsigned short>(const unsigned short& s) { Int(s); }
+template<> ALWAYS_INLINE void JsonSimpleWriter::Value<short>(const short& s) { Int(s); }
+template<> ALWAYS_INLINE void JsonSimpleWriter::Value<int>(const int& i) { Int(i); }
+template<> ALWAYS_INLINE void JsonSimpleWriter::Value<bool>(const bool& b) { Bool(b); }
+template<> ALWAYS_INLINE void JsonSimpleWriter::Value<const char*>(const char* const& s) { wr.String(s); }
 
 }
